eb1a3f349b6ab5316837513c014131b0c2b9cb0e
Jorge Luis <sejidjorge@gmail.com>
2023-03-08 13:56:19 -0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { QrReader } from 'react-qr-reader';
+import React from "react";
+import ReactDOM from "react-dom";
+import { QrReader } from "react-qr-reader";
 
-import './index.css';
+import "./index.css";
 
 function App() {
+  const [readCode, setReadCode] = React.useState<string>("");
   const handleScan = (data: string | null) => {
-    alert(data);
-  }
-
-  const handleError = (err: any) => {
-    console.error(err);
-  }
+    setReadCode(data);
+  };
 
   return (
-    <div>
+    <div style={{ maxWidth: "300px" }}>
       <QrReader
         delay={300}
-        onError={handleError}
-        onScan={handleScan}
-        style={{ width: '100%' }}
-        facingMode="environment"
+        onResult={(result, error) => {
+          if (!!result) {
+            setReadCode(result?.text);
+          }
+
+          if (!!error) {
+            console.info(error);
+          }
+        }}        style={{ width: "100%" }}
+        constraints={{
+          facingMode: "environment",
+        }}
       />
+      <code style={{ color: "red" }}>Code: {readCode}</code>
     </div>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));''
+ReactDOM.render(<App />, document.getElementById("root"));
+("");
